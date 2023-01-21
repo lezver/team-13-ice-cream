03a187db7c21b1f9925682398538ea69b4284e48
RenTensei <scs.bon7@gmail.com>
2023-01-21 18:07:51 +0200

    init reviews

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,26 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 50,
   loop: true,
+
+  // simulateTouch: true,
+  touchRatio: 2,
+  grabCursor: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    type: 'bullets',
+    clickable: true,
   },
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 10000,
+    stopOnLastSlide: false,
   },
 });
