5f70145e44772fb46f1f6a2142abe6a0430a9726
RenTensei <scs.bon7@gmail.com>
2023-01-22 20:31:40 +0200

    init customer reviews, init gallery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,66 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
+const swiper = new Swiper('.swiper-reviews', {
+  slidesPerView: 1,
+  spaceBetween: 10,
   loop: true,
+  simulateTouch: true,
+  touchRatio: 2,
+  grabCursor: true,
 
+  breakpoints: {
+    480: {
+      spaceBetween: -95,
+    },
+    610: {
+      spaceBetween: -160,
+    },
+    768: {
+      spaceBetween: 0,
+    },
+    1200: {
+      spaceBetween: -100,
+    },
+    1440: {
+      spaceBetween: -200,
+      touchRatio: 3,
+    },
+  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    type: 'bullets',
+    clickable: true,
   },
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+  },
+});
+
+const swiperGallery = new Swiper('.swiper-gallery', {
+  speed: 1000,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  effect: 'fade',
+  simulateTouch: true,
+  touchRatio: 3,
+  grabCursor: true,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
   },
 });
