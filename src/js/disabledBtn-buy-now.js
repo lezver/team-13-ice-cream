87591790dcfcda7dc33f3acb1a61c2ab0f11bd20
Vladislav <102545184+lezver@users.noreply.github.com>
2023-01-27 12:18:47 +0200

    add disable and valid affect

---
+++ src/js/disabledBtn-buy-now.js
@@ -0,0 +1,20 @@
+const form = document.forms.form1;
+
+form.addEventListener('change', function () {
+  this.btnSubmit.disabled = this.checkValidity() ? false : true;
+});
+
+Array.from(form.elements).forEach(inp => {
+  if (inp.required) {
+    inp.addEventListener('change', () => {
+      if (inp.checkValidity()) {
+        inp.classList.remove('invalid');
+        inp.classList.add('valid');
+      } else {
+        inp.classList.remove('valid');
+        inp.classList.add('invalid');
+        inp.reportValidity();
+      }
+    });
+  }
+});
